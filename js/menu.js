88a9302e958c56423d4411c7210718803937a80e
denisyuk26 <sergey.denisyuk@gmail.com>
2017-01-13 11:39:53 +0200

    second

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,18 @@
-var menumob='close';
-
+var menumob="close";
 
 $(document).ready(function(){
-    $('.menu-mob').on('click', showMmenu);
+    $(".menu-mob").on("click", showMmenu);
+    $("#openmap").on("click", openMap);
+    $("#closemap").on("click", closeMap);
 
-
+    
     $("form").submit(function() {
         $.ajax({
             type: "GET",
             url: "email.php",
             data: $("form").serialize()
-        }).done(function() {
+        })
+            .done(function() {
             alert("Спасибо за заявку!");
             setTimeout(function() {
                 $.fancybox.close();
@@ -19,21 +21,55 @@ $(document).ready(function(){
         return false;
     });
 
+    function openMap() {
+        if (menumob =="close"){
+            $("#myMap").css ({
+                "position": "relative"
+            });
+            $(".form-wrap").css ({
+                "display": "none"
+            });
+
+            $(".closemap").animate ({
+                "opacity": "1"
+            }, 500);
+            menumob = "open";
+        }
+
+    }
+
+    function closeMap() {
+        $("#myMap").css({
+            "position": "absolute"
+
+        });
+        $(".form-wrap").css ({
+            "display": "block"
+        });
+
+        $(".closemap").animate ({
+            "opacity": "0"
+        }, 500);
+
+        menumob = "close";
+    }
+
+
 
     function showMmenu(){
-        if (menumob =='close') {
+        if (menumob =="close") {
             $(this).animate({
                 'top': 0
             },500);
-            menumob = 'open';
+            menumob = "open";
             $('.open').fadeOut(250);
             $('.close').fadeIn(250);
         }
         else {
             $(this).animate({
-                'top': '-55px'
+                "top": "-255px"
             },500);
-            menumob = 'close';
+            menumob = "close";
             $('.open').fadeIn(250);
             $('.close').fadeOut(250);
         }
